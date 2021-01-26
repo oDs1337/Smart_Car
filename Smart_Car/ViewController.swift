c193e97dc9889502147b54cd7bbde212416e07a2
oDs1337 <tobias.contact@gmail.com>
2021-01-26 21:31:55 +0100

    Add connection between UI and code

--- Smart_Car/ViewController.swift
+++ Smart_Car/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //  connect labels
+    //  fuel constumption
+    @IBOutlet weak var labelFuelConsumption: UILabel!
+    
+    //  distance
+    @IBOutlet weak var labelDistance: UILabel!
+    
+    //  result
+    @IBOutlet weak var labelResult: UILabel!
+    
+    
+    //  connect text fields
+    //  fuel text field
+    @IBOutlet weak var fuelConsumptionTextField: UITextField!
+    
+    //  distance text field
+    @IBOutlet weak var distanceTextField: UITextField!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    //  connect options
+    //  fuel options
+    @IBAction func fuelOptions(_ sender: Any) {
+    }
+    
+    //  distance options
+    @IBAction func distanceOptions(_ sender: Any) {
+    }
+    
+    //  connect buttons
+    @IBAction func submitButton(_ sender: Any) {
+    }
+    
+    
 
 }
 
