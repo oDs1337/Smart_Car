6d44d7444db66d0a006616fb34f4a1e020ea5184
oDs1337 <tobias.contact@gmail.com>
2021-02-25 16:58:11 +0100

    Add working change view between data and cars thru SG

--- Smart_Car/HistoryController.swift
+++ Smart_Car/HistoryController.swift
@@ -6,20 +6,76 @@
 //
 
 import UIKit
+import GoogleMobileAds
+import GoogleUtilities
+import CloudKit
+
 
 class HistoryController: UIViewController {
 
+    @IBOutlet weak var viewContainer: UIView!
+    
+    let dataVC = Smart_Car.dataViewController()
+    let carVC = Smart_Car.carViewController()
+    
+    @IBOutlet weak var bannerView: GADBannerView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
+        //  ad managment
+        let appId = "ca-app-pub-2859570082554006/1576697552"
+        //let testAppId = "ca-app-pub-3940256099942544/2934735716"
+        bannerView.adUnitID = appId
+        bannerView.rootViewController = self
+        bannerView.load(GADRequest())
+        bannerView.delegate = self
+         
         
-        
-        
+        setup()
         // Do any additional setup after loading the view.
     }
     
-
+    private func setup()
+    {
+        
+        
+        addChild(dataVC)
+        addChild(carVC)
+        
+        viewContainer.addSubview(dataVC.view)
+        viewContainer.addSubview(carVC.view)
+        
+        dataVC.didMove(toParent: self)
+        carVC.didMove(toParent: self)
+        
+        dataVC.view.frame = self.view.bounds
+        carVC.view.frame = self.view.bounds
+        carVC.view.isHidden = true
+    }
+    
+    @IBAction func switchViewAction(_ sender: UISegmentedControl) {
+        
+        
+        dataVC.view.isHidden = true
+        carVC.view.isHidden = true
+        
+        switch sender.selectedSegmentIndex
+        {
+        case 0:
+            viewContainer.bringSubviewToFront(dataVC.view)
+            dataVC.view.isHidden = false
+            break
+        case 1:
+            viewContainer.bringSubviewToFront(carVC.view)
+            carVC.view.isHidden = false
+            break
+        default:
+            break
+        }
+    }
+    
+    
     /*
     // MARK: - Navigation
 
